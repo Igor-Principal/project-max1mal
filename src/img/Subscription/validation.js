a0f2437896d92b3d70fbd3b515c0563ac50dfab4
Vitalii Kravchenko <opabbk@gmail.com>
2023-08-25 08:04:38 +0300

    add-validation-script

---
+++ src/img/Subscription/validation.js
@@ -0,0 +1,23 @@
+const inputName = document.getElementById("user-name");
+const inputEmail = document.getElementById("user-email");
+const errorNameIcon = document.querySelector(".name-error-icon");
+const errorEmailIcon = document.querySelector(".email-error-icon")
+
+
+inputName.addEventListener("change", function() {
+  if (!this.checkValidity()) {
+    errorNameIcon.style.display = "block";
+    this.classList.add("name-error");
+  } else {
+    errorNameIcon.style.display = "none";
+    this.classList.remove("name-error");
+  }
+});
+
+inputEmail.addEventListener("input", function() {
+  if (!this.checkValidity()) {
+    errorEmailIcon.style.display = "block";
+  } else {
+    errorEmailIcon.style.display = "none";
+  }
+});
