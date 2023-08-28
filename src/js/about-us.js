a8194eee2e50886b02dfefb1666d41c8a3a310c5
kiljestyn <137730997+kiljestyn@users.noreply.github.com>
2023-08-28 12:05:28 +0300

    about-us script for numbers

---
+++ src/js/about-us.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 5, 3000);
+    counter("count2", 49, 100, 5000);
+    counter("count3", 251, 500, 5000);
+});
